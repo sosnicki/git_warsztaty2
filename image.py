882aebb1069d92a97ab57e7d39aeff600214f71e
Tomasz Sosnicki <tom.sosnicki@gmail.com>
2025-01-07 12:35:07 +0100

    ala: Ostrość z Laplace'a

--- image.py
+++ image.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
+import numpy as np
 from PIL import Image as PILImage
+from scipy.ndimage import laplace
+
 
 class Image:
     def __init__(self, path: str | Path = None):
@@ -13,8 +16,19 @@ class Image:
 
     @property
     def sharpness(self) -> float:
-        # TODO: Funkcja zwraca ocenę ostrości obrazu w postci liczby rzeczywistej
-        ...
+        # Konwersja obrazu na skalę szarości
+        image = self.image.convert("L")
+
+        # Konwersja obrazu do tablicy numpy
+        image_array = np.array(image, dtype=np.float32)
+
+        # Obliczenie Laplacjana
+        laplacian = laplace(image_array)
+
+        # Obliczenie wariancji Laplacjana
+        sharpness = laplacian.var()
+
+        return sharpness
 
     def histogram(self, path: str | Path):
         # TODO: Funkcja tworzy histogram i zapisuje go w ścieżce path
