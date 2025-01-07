15d93d1fa2772d11b5c1f8c90402d3e48e688249
Kuba <kkubajan09@gmail.com>
2025-01-07 13:09:27 +0100

    jt/koniecSwiataPoprawa

--- image.py
+++ image.py
@@ -54,7 +54,7 @@ class Image:
 
     def histogram(self, path: str | Path):
         # TODO: Funkcja tworzy histogram i zapisuje go w ścieżce path
-        pritn("Koniec Świata")
+        prints("Koniec Świata")
 
     def blur(self):
         # TODO: Funkcja rozmywa obraz metodą np. Gaussa i podmienia zmienną self.image
